4876b113e32c376c3d85ff5717f74216591432ad
Joelb712 <chichobarboza.jb7@gmail.com>
2025-08-19 16:34:37 -0300

    form ajax

--- Productos/views.py
+++ Productos/views.py
@@ -1,22 +1,39 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, get_object_or_404, redirect, HttpResponse
 from .models import Producto
 from .forms import ProductoForm
 from django.contrib import messages
+from django.http import JsonResponse
+from django.views.decorators.clickjacking import xframe_options_exempt
 
 def lista_productos(request):
     productos = Producto.objects.all()
     return render(request, 'productos.html', {'productos': productos})
 
+# def crear_producto(request):
+#     if request.method == 'POST':
+#         form = ProductoForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, 'Producto creado correctamente.')
+#             return redirect('lista_productos')
+#     else:
+#         form = ProductoForm()
+#     return render(request, 'form.html', {'form': form, 'titulo': 'Nuevo Producto'})
+
+@xframe_options_exempt
 def crear_producto(request):
     if request.method == 'POST':
         form = ProductoForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Producto creado correctamente.')
-            return redirect('lista_productos')
+            # Avisar al iframe que debe cerrarse:
+            return HttpResponse(
+                "<script>window.parent.postMessage({action: 'closeBootbox'}, '*');</script>"
+            )
     else:
         form = ProductoForm()
-    return render(request, 'form.html', {'form': form, 'titulo': 'Nuevo Producto'})
+    return render(request, 'form.html', {'form': form})
+
 
 def editar_producto(request, pk):
     producto = get_object_or_404(Producto, pk=pk)
